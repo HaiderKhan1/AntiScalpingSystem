f14c2b6ba74104804702824841bf2552da595f23
Haider Khan <hkhan24@uoguelph.ca>
2021-08-14 18:04:44 -0400

    Finished sqlserver_config

--- app.py
+++ app.py
@@ -3,9 +3,19 @@ from flask import Flask, render_template, url_for, redirect, request
 app = Flask(__name__)
 key = "b'1kzTkTXWNjrV3wHfAZUKIJI8CkvzwB0nmVH_S10lpe0='"
 
-@app.route("/", methods = ['GET', 'POST'])
+@app.route("/")
 def main_page():
     return render_template("home.html")
+
+@app.route("/post_data", methods = ['GET', 'POST'])
+def process_data():
+    upc_num = request.form["upc"]
+    cc_num = request.form["cc"]
+    
+    #check weather this exists in the database
+    return render_template("home.html")
+
+    
 
 if __name__ == "__main__":
     app.run(debug = True)
--- sqlserver_config.py
+++ sqlserver_config.py
@@ -2,19 +2,50 @@ import pyodbc
 import random
 from cryptography.fernet import Fernet
 
-# server = 'personalprojectdb.database.windows.net'
-# database = 'AntiScalpingSystemDB'
-# username = 'Haider'
-# password = '{786Admin}'   
-# driver= '{ODBC Driver 17 for SQL Server}'
+server = 'personalprojectdb.database.windows.net'
+database = 'AntiScalpingSystemDB'
+username = 'Haider'
+password = '{786Admin}'   
+driver= '{ODBC Driver 17 for SQL Server}'
 
-# db = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
-# cursor = db.cursor()
-# cursor.execute("CREATE TABLE USERDATA (upc INT NOT NULL, cc_number VARCHAR(60) NOT NULL, transaction_date DATETIME NOT NULL, PRIMARY KEY(cc_number));")
+# initalize the cryptography library
+key = "1kzTkTXWNjrV3wHfAZUKIJI8CkvzwB0nmVH_S10lpe0="
+fernet = Fernet(key)
+
+#initalize database
+db = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = db.cursor()
+
+# #create the database table
+# cursor.execute("CREATE TABLE TRANSACTIONDATA (upc VARCHAR(12) NOT NULL, cc_number NVARCHAR(1000) NOT NULL, transaction_date DATE NOT NULL, PRIMARY KEY(cc_number));")
+
 
 for i in range(0,100):
+    #generate random upc and cc
     upc = str(random.randint(100000000000,999999999999))
     cc = str(random.randint(1000000000000000,9999999999999999))
-    print("%s - %s"%(upc, cc))
+    cc_encrypted = fernet.encrypt(cc.encode())
+    
+    #prepare credit card number to be pushed to the database
+    char_cc = list(cc_encrypted.decode())
+    char_cc.insert(0,"'")
+    char_cc.insert(len(char_cc), "'")
+    cc_encrypted = ''.join(char_cc)
+
+    #prepare upc to be pushed to the db
+    db_upc = list(upc)
+    db_upc.insert(0, "'")
+    db_upc.insert(len(char_cc), "'")
+    upc = "".join(db_upc)
+
+    #push info to the database
+    query = "INSERT INTO TRANSACTIONDATA (upc, cc_number, transaction_date) VALUES ({}, {}, '2021-08-14')".format(upc, cc_encrypted)
+    cursor.execute(query)
+    db.commit()
+
+
+
+
+
 
  
